448acaf2823b4462ef460435311ba9a3706f891b
brynjar <brynjar.ulriksen@miles.no>
2022-09-05 15:24:46 +0200

    add strikethrough on older conferences

--- src/components/Conference.svelte
+++ src/components/Conference.svelte
@@ -1,10 +1,28 @@
 <script>
 	export let conference;
 	export let active;
+	import cs from './../routes/_conferences';
+
+
+	function isBeforeToday(date) {
+		const today = new Date();
+
+		today.setHours(0, 0, 0, 0);
+
+		return date < today;
+	}
+	let date = conference.enddateformat.split(",");
+    
+
+	let conferenceDate = new Date(date[0], date[1], date[2]);
+	conferenceDate.setHours(0, 0, 0, 0);
+
+	let oldConference = isBeforeToday(conferenceDate);
+	console.log(conference.title, conferenceDate, oldConference);
 </script>
 
 <a href="konferanser/{conference.title}">
-	<div>
+	<div class:oldConference>
 		{conference.slug}
 		{#if active}
 			{conference.startdate} - {conference.enddate}
@@ -13,4 +31,7 @@
 </a>
 
 <style>
+	.oldConference {
+		text-decoration: line-through;
+	}
 </style>
--- src/routes/_conferences.js
+++ src/routes/_conferences.js
@@ -3,6 +3,7 @@ const conferences =[
     slug:"JavaZone",
     startdate: "7. september",
     enddate: '8. september',
+    enddateformat: "2022,8,8",
     logosrc: './javazonelogo.png',
     link: 'https://2022.javazone.no/' 
 },
@@ -10,6 +11,7 @@ const conferences =[
 slug:"Booster",
 startdate: "7. Mai",
 enddate: '8. Mai',
+enddateformat: "2022,4,8",
 logosrc: 'boosterlogo.png', 
 link: 'https://2022.boosterconf.no/'   
 },
