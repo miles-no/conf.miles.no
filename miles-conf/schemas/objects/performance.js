2cfae0b6de8c0b75168443c56df845c8def138ce
Yngve Sundfjord <yngve.sundfjord@miles.no>
2022-10-17 15:56:15 +0200

    Add nice preview of performance data

--- miles-conf/schemas/objects/performance.js
+++ miles-conf/schemas/objects/performance.js
@@ -24,7 +24,20 @@ export default {
       name: 'performanceUrls',
       type: 'array',
       of: [{ type: 'urls' }]
+    }
+  ],
+  preview: {
+    select: {
+      title: 'submission.title',
+      dateAndTime: 'dateAndTime',
+      location: 'location'
     },
-
-  ]
+    prepare(selection) {
+      const { title, dateAndTime, location } = selection;
+      return {
+        title: title,
+        subtitle: `${location ? location+' - ' : ''}${new Intl.DateTimeFormat('no-NO', { dateStyle: 'medium', timeStyle: 'short', timeZone: 'Etc/UTC'}).format(new Date(dateAndTime))}`
+      }
+    }
+  }
 }
