f9c4f0b688b35c09bff9e370b0e4284a76f906e9
Teis Lindemark <teis.lindemark@gmail.com>
2023-02-08 09:57:50 +0100

    Update +server.js

--- src/routes/login/+server.js
+++ src/routes/login/+server.js
@@ -1,26 +1,20 @@
-//import * as queryString from 'query-string';
+import queryString from 'query-string';
 import { redirect } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
  
 /** @type {import('./$types').RequestHandler} */
 export async function GET(event) {
-    //const stringifiedParams = queryString.stringify({
-    //    client_id: env.GOOGLE_CLIENT_ID,
-    //    redirect_uri: `${env.ORIGIN}/oauth2/callback/google`,
-    //    scope: [
-    //        'https://www.googleapis.com/auth/userinfo.email',
-    //        'https://www.googleapis.com/auth/userinfo.profile',
-    //    ].join(' '),
-    //    response_type: 'code',
-    //    access_type: 'offline',
-    //    prompt: 'consent'
-    //});
-    //const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
-    const redirectUri = `${env.ORIGIN}/oauth2/callback/google`;
-    const scope = scope: [
-        'https://www.googleapis.com/auth/userinfo.email',
-        'https://www.googleapis.com/auth/userinfo.profile',
-    ].join(' ')
-    const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code&access_type=offline&prompt=consent`;
+    const stringifiedParams = queryString.stringify({
+        client_id: env.GOOGLE_CLIENT_ID,
+        redirect_uri: `${env.ORIGIN}/oauth2/callback/google`,
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.email',
+            'https://www.googleapis.com/auth/userinfo.profile',
+        ].join(' '),
+        response_type: 'code',
+        access_type: 'offline',
+        prompt: 'consent'
+    });
+    const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
     throw redirect(307, googleLoginUrl);
 }
