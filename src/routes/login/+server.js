81ef39ad5eab695732d486a451d44fb2efd59e91
Teis Lindemark <teis.lindemark@gmail.com>
2023-02-08 08:18:59 +0100

    Update +server.js

--- src/routes/login/+server.js
+++ src/routes/login/+server.js
@@ -6,17 +6,25 @@ import { env } from '$env/dynamic/private';
 // export function load({ locals }) {
 /** @type {import('./$types').RequestHandler} */
 export async function GET(event) {
-    const stringifiedParams = queryString.stringify({
-        client_id: env.GOOGLE_CLIENT_ID,
-        redirect_uri: 'http://localhost:3000/oauth2/callback/google',
-        scope: [
-            'https://www.googleapis.com/auth/userinfo.email',
-            'https://www.googleapis.com/auth/userinfo.profile',
-        ].join(' '),
-        response_type: 'code',
-        access_type: 'offline',
-        prompt: 'consent'
-    });
-    const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
-    throw redirect(307, googleLoginUrl);
+    try {
+     const stringifiedParams = queryString.stringify({
+         client_id: env.GOOGLE_CLIENT_ID,
+         redirect_uri: 'http://localhost:3000/oauth2/callback/google',
+         scope: [
+             'https://www.googleapis.com/auth/userinfo.email',
+             'https://www.googleapis.com/auth/userinfo.profile',
+         ].join(' '),
+         response_type: 'code',
+         access_type: 'offline',
+         prompt: 'consent'
+     });
+     const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
+     throw redirect(307, googleLoginUrl);
+    } catch(error) {
+     return {
+      success: false,
+      error: error,
+      env: env
+     }
+    }
 }
