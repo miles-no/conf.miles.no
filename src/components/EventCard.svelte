b518b594a2300a8bca70c1fcfd89914908370f5d
Johanna Kinstad <johannakinstad@Johannas-MacBook-Pro.local>
2023-03-09 14:55:52 +0100

    added coverphoto

--- src/components/EventCard.svelte
+++ src/components/EventCard.svelte
@@ -11,9 +11,17 @@
 	} from '@smui/card';
 	import Button, { Label } from '@smui/button';
 	import IconButton, { Icon } from '@smui/icon-button';
+	import imageUrlBuilder from '@sanity/image-url';
+	import { client } from '$lib/sanityClient';
 
 	export let event;
 	const date = formatConferenceDateRange(event.startDate, event.endDate);
+
+	const builder = imageUrlBuilder(client);
+
+	function urlFor(source) {
+		return builder.image(source);
+	}
 </script>
 
 <div class="card-container">
@@ -21,9 +29,13 @@
 		<Media class="card-media-16x9" aspectRatio="16x9">
 			<MediaContent
 				><img
-					style="width: 100%;"
-					src="https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej.png"
+					style="width: 100%; height: 100%;"
 					alt=""
+					src={event.imageUrl
+						? urlFor(event.imageUrl).size(500, 300).quality(100).url()
+						: 'https://www.miles.no/wp-content/uploads/2020/11/PT6A3984-kopi.jpg'}
+					height="300"
+					width="500"
 				/></MediaContent
 			>
 		</Media>
