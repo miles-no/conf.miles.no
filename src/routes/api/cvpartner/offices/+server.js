5f1a536c777ea42682ba5acd1b021335f19ab7e7
Johanna Kinstad <johannakinstad@Johannas-MacBook-Pro.local>
2023-03-09 12:23:20 +0100

    redesign for submission page

--- src/routes/api/cvpartner/offices/+server.js
+++ src/routes/api/cvpartner/offices/+server.js
@@ -1,16 +1,16 @@
 import { env } from '$env/dynamic/private';
 import { json } from '@sveltejs/kit';
 import { fetchOffices } from '$lib/server/cvpartnerClient';
- 
+
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ request }) {
-  const authHeader = request.headers.get('Authorization');
-  if(authHeader !== env.CONF_API_KEY) {
-    return new Response(JSON.stringify({message: 'Invalid credentials'}), {status: 401});
-  }
-  const officesTotal = await fetchOffices();
-  return json({
-    total: officesTotal.length,
-    offices: officesTotal
-  });
+	const authHeader = request.headers.get('Authorization');
+	if (authHeader !== env.CONF_API_KEY) {
+		return new Response(JSON.stringify({ message: 'Invalid credentials' }), { status: 401 });
+	}
+	const officesTotal = await fetchOffices();
+	return json({
+		total: officesTotal.length,
+		offices: officesTotal
+	});
 }
--- src/routes/konferanser/[konferanse]/agenda/[slug]/+page.server.js
+++ src/routes/konferanser/[konferanse]/agenda/[slug]/+page.server.js
@@ -1,23 +1,28 @@
 import { fetchConferencePerformance } from '$lib/sanityClient';
+import { getUserFromCookie } from '$lib/server/auth';
+import { redirect } from '@sveltejs/kit';
 
 export const prerender = false;
 export const ssr = false;
 export const csr = true;
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ params }) {
-    const { konferanse, slug } = params;
-    const conference = await fetchConferencePerformance(konferanse, slug);
+export async function load({ params, cookies }) {
+	const { konferanse, slug } = params;
+	const conference = await fetchConferencePerformance(konferanse, slug);
+	const user = getUserFromCookie(cookies.get('session'));
+	if (!conference.conference || !conference.conference.performance) {
+		return {
+			status: 404
+		};
+	}
+	if (!user.isAuthenticated && !conference.conference.showExternally) {
+		throw redirect(307, '/');
+	}
 
-    if (!conference.conference || !conference.conference.performance) {
-        return {
-            status: 404
-        };
-    }
-
-    return {
-        conference: conference.conference,
-        performance: conference.conference.performance,
-        submission: conference.conference.performance.submission
-    };
+	return {
+		conference: conference.conference,
+		performance: conference.conference.performance,
+		submission: conference.conference.performance.submission
+	};
 }
