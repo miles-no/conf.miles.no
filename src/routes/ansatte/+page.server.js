ddfd09a6cb3ff3133c48d3b75b7261043279f355
Johanna Kinstad <johannakinstad@Johannas-MacBook-Pro.local>
2023-03-14 12:16:41 +0100

    testing without users

--- src/routes/ansatte/+page.server.js
+++ src/routes/ansatte/+page.server.js
@@ -5,14 +5,14 @@ export const ssr = false;
 export const csr = true;
 
 export async function load() {
-	// const officesUrl = 'http://127.0.0.1:3000/api/cvpartner/offices';
-	// const usersUrl = 'http://127.0.0.1:3000/api/cvpartner/users';
-	// const responseOffices = await fetch(officesUrl, {
-	// 	mode: 'same-origin',
-	// 	headers: {
-	// 		Authorization: `${env.CONF_API_KEY}`
-	// 	}
-	// });
+	const officesUrl = 'http://127.0.0.1:3000/api/cvpartner/offices';
+	const usersUrl = 'http://127.0.0.1:3000/api/cvpartner/users';
+	const responseOffices = await fetch(officesUrl, {
+		mode: 'same-origin',
+		headers: {
+			Authorization: `${env.CONF_API_KEY}`
+		}
+	});
 
 	// const responseUsers = await fetch(usersUrl, {
 	// 	mode: 'same-origin',
@@ -21,15 +21,15 @@ export async function load() {
 	// 	}
 	// });
 
-	// const offices = await responseOffices.json();
+	const offices = await responseOffices.json();
 	// const users = await responseUsers.json();
 
-	// const data = {
-	// 	offices: offices,
-	// 	users: users
-	// };
+	const data = {
+		offices: offices
+		// users: users
+	};
 	return {
-		// offices: data.offices.offices,
+		offices: data.offices.offices,
 		// users: data.users.users,
 		text: 'dette er tekst på ansatt side'
 	};
