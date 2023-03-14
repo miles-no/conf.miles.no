83eaec5320e42e2a8c840ddd0826a33648eb1b78
Johanna Kinstad <johannakinstad@Johannas-MacBook-Pro.local>
2023-03-14 12:36:01 +0100

    latest release

--- src/routes/ansatte/+page.server.js
+++ src/routes/ansatte/+page.server.js
@@ -7,12 +7,12 @@ export const csr = true;
 export async function load() {
 	const officesUrl = 'http://127.0.0.1:3000/api/cvpartner/offices';
 	const usersUrl = 'http://conf.miles.no/api/cvpartner/users';
-	// const responseOffices = await fetch(officesUrl, {
-	// 	mode: 'same-origin',
-	// 	headers: {
-	// 		Authorization: `${env.CONF_API_KEY}`
-	// 	}
-	// });
+	const responseOffices = await fetch(officesUrl, {
+		mode: 'same-origin',
+		headers: {
+			Authorization: `${env.CONF_API_KEY}`
+		}
+	});
 
 	const responseUsers = await fetch(usersUrl, {
 		mode: 'same-origin',
@@ -21,15 +21,15 @@ export async function load() {
 		}
 	});
 
-	// const offices = await responseOffices.json();
+	const offices = await responseOffices.json();
 	const users = await responseUsers.json();
 
 	const data = {
-		// offices: offices
+		offices: offices
 		users: users
 	};
 	return {
-		// offices: data.offices.offices,
+		offices: data.offices.offices,
 		users: data.users.users,
 		text: 'dette er tekst på ansatt side'
 	};
