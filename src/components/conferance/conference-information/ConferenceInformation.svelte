a0f7c16debe4692751b9ae6a3c41e0253946ce85
Nacha Nanthawisit <nanthawisit.natacha@gmail.com>
2023-05-11 14:48:27 +0200

    using Paper in conference information

--- src/components/conferance/conference-information/ConferenceInformation.svelte
+++ src/components/conferance/conference-information/ConferenceInformation.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Paper, { Content } from '@smui/paper';
 	import { Icon } from '@smui/button';
 	import type { IExternalConference } from '../../../model/external-conference';
 	import { formatConferenceDateRange } from '$lib';
@@ -8,47 +9,53 @@
 	const date = formatConferenceDateRange(conference?.startDate, conference?.endDate);
 </script>
 
-<div class="conference-information-tag grey-text">
-	<div>
-		<p aria-label={`dato: ${date}`}>
-			<Icon class="material-icons" alt="">event</Icon>
-			{date}
-		</p>
-	</div>
-	<div>
-		<p aria-label={`sted: ${conference?.location}`}>
-			<Icon class="material-icons" alt="">location_on</Icon>
-			{conference?.location}
-		</p>
-	</div>
-	<div>
-		<p aria-label={`billetttype: ${conference?.tickettype}`}>
-			<Icon class="material-icons" alt="">notifications_active</Icon>
-			{conference?.tickettype}
-		</p>
-	</div>
-	<div>
-		<p>
-			<Icon class="material-icons" alt="">link</Icon>
-			<a aria-label={`lenke til ${conference?.title}`} href={conference?.url}>
-				{conference?.url}
-			</a>
-		</p>
-	</div>
-	<div>
-		<p aria-label={`pris: ${conference?.price}`}>
-			<Icon class="material-icons" alt="">payments</Icon>
-			{conference?.price}
-		</p>
-	</div>
-</div>
+<Paper variant="unelevated">
+	<Content class="conference-information-tag grey-text">
+		<div>
+			<p aria-label={`dato: ${date}`}>
+				<Icon class="material-icons" alt="">event</Icon>
+				{date}
+			</p>
+		</div>
+		<div>
+			<p aria-label={`sted: ${conference?.location}`}>
+				<Icon class="material-icons" alt="">location_on</Icon>
+				{conference?.location}
+			</p>
+		</div>
+		<div>
+			<p aria-label={`billetttype: ${conference?.tickettype}`}>
+				<Icon class="material-icons" alt="">notifications_active</Icon>
+				{conference?.tickettype}
+			</p>
+		</div>
+		<div>
+			<p>
+				<Icon class="material-icons" alt="">link</Icon>
+				<a aria-label={`lenke til ${conference?.title}`} href={conference?.url}>
+					{conference?.url}
+				</a>
+			</p>
+		</div>
+		<div>
+			<p aria-label={`pris: ${conference?.price}`}>
+				<Icon class="material-icons" alt="">payments</Icon>
+				{conference?.price}
+			</p>
+		</div>
+	</Content>
+</Paper>
 
 <style lang="scss">
+	p {
+		margin: 0;
+	}
+
 	// Mobile
-	.conference-information-tag {
+	:global(.conference-information-tag) {
 		font-weight: 600;
 		letter-spacing: 1px;
-		p {
+		:global(p) {
 			display: flex;
 			gap: 0.5rem;
 			align-items: center;
@@ -57,7 +64,7 @@
 
 	// Desktop
 	@media (min-width: 850px) {
-		.conference-information-tag {
+		:global(.conference-information-tag) {
 			display: grid;
 			grid-template-columns: repeat(auto-fill, minmax(11rem, 1fr));
 		}
