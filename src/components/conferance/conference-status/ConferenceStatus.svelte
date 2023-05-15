3acbf4c349f37f020d7121fa1815f2c6f9abbb84
Nacha Nanthawisit <nanthawisit.natacha@gmail.com>
2023-05-15 12:51:22 +0200

    creating component for conference status

---
+++ src/components/conferance/conference-status/ConferenceStatus.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import Select, { Option } from '@smui/select';
+	import { Status, type StatusType } from '../../../enums/status';
+
+	const statusEntries = Object.entries(Status);
+	export let selectedStatus: StatusType | undefined = undefined;
+</script>
+
+<div class="conference-status">
+	<p>Min status:</p>
+	<Select
+		class="conference-status-select"
+		variant="outlined"
+		label="Velg status"
+		bind:value={selectedStatus}
+	>
+		<Option value="" />
+		{#each statusEntries as [statusKey, statusValue]}
+			<Option value={statusKey}>{statusValue}</Option>
+		{/each}
+	</Select>
+</div>
+
+<style lang="scss">
+	p {
+		margin-bottom: 0;
+	}
+
+	.conference-status {
+		display: flex;
+		gap: 0.7rem;
+		align-items: center;
+		font-weight: 600;
+
+		:global(.conference-status-select) {
+			width: 70%;
+			background-color: #ffcd2957;
+			border-radius: 5px;
+			:global(.mdc-select__anchor) {
+				height: 2.5rem;
+			}
+			:global(label) {
+				color: #000 !important;
+				letter-spacing: 1px;
+			}
+		}
+	}
+</style>
--- src/routes/konferanser/ekstern/[slug]/+page.svelte
+++ src/routes/konferanser/ekstern/[slug]/+page.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
-	import Select, { Option } from '@smui/select';
 	import ConferenceInformation from '../../../../components/conferance/conference-information/ConferenceInformation.svelte';
 	import type { IPageLoadData } from './+page.server';
 	import { Status } from '../../../../enums/status';
 	import type { StatusType } from '../../../../enums/status';
 	import ConferenceCategoryTag from '../../../../components/tag/conference-category-tag/ConferenceCategoryTag.svelte';
+	import ConferenceStatus from '../../../../components/conferance/conference-status/ConferenceStatus.svelte';
 
 	export let data: IPageLoadData | undefined = undefined;
 	export let externalConference = data?.conference;
 	export let user = data?.user;
 
-	const statusEntries = Object.entries(Status);
 	let selectedStatus: StatusType | undefined = undefined;
 </script>
 
@@ -18,8 +17,8 @@
 	<title>{externalConference?.title} - Miles</title>
 </svelte:head>
 
-<div class="conference-details">
-	<img class="conference-details-image" src={externalConference?.imageUrl} alt="" />
+<div>
+	<img src={externalConference?.imageUrl} alt="" />
 	<div class="conference-details-content">
 		<div class="conference-details-content-info">
 			<div class="conference-details-content-info-content">
@@ -37,20 +36,7 @@
 				{/if}
 			</div>
 			<div class="conference-details-content-info-status">
-				<div class="conference-details-content-info-status-my-status">
-					<p>Min status:</p>
-					<Select
-						class="conference-details-content-info-status-select"
-						variant="outlined"
-						label="Velg status"
-						bind:value={selectedStatus}
-					>
-						<Option value="" />
-						{#each statusEntries as [statusKey, statusValue]}
-							<Option value={statusKey}>{statusValue}</Option>
-						{/each}
-					</Select>
-				</div>
+				<ConferenceStatus bind:selectedStatus />
 				<div class="conference-details-content-info-status-registered">
 					<div>
 						<h2 class="visuallyhidden">Alle som er interessert</h2>
@@ -95,7 +81,7 @@
 		@include visuallyhidden();
 	}
 
-	//mobile
+	// Mobile
 	h1 {
 		font-weight: 600;
 	}
@@ -108,122 +94,99 @@
 		margin-bottom: 0;
 	}
 
-	.conference-details-content-info-content {
-		grid-area: information;
+	img {
+		height: 100%;
+		width: 100%;
+		object-fit: cover;
 	}
+
+	.conference-details-content {
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		gap: 1rem;
+	}
+
+	.conference-details-content-info {
+		display: flex;
+		flex-direction: column;
+		gap: 1.5rem;
+	}
+
 	.conference-details-content-info-status {
-		grid-area: status;
+		display: flex;
+		flex-direction: column;
+		gap: 1.2rem;
+		overflow: visible;
 	}
+
 	.conference-details-content-description {
-		grid-area: description;
-	}
-
-	.conference-details {
-		.conference-details-image {
-			height: 100%;
-			width: 100%;
-			object-fit: cover;
-		}
-
-		.conference-details-content {
-			display: flex;
-			flex-direction: column;
-			padding: 1rem;
-			gap: 1rem;
-		}
-
-		.conference-details-content-info {
-			display: flex;
-			flex-direction: column;
-			gap: 1.5rem;
-		}
-
-		.conference-details-content-info-status {
-			display: flex;
-			flex-direction: column;
-			gap: 1.2rem;
-			overflow: visible;
-
-			.conference-details-content-info-status-my-status {
-				display: flex;
-				gap: 0.7rem;
-				align-items: center;
-				font-weight: 600;
-			}
-
-			:global(.conference-details-content-info-status-select) {
-				width: 70%;
-				background-color: #ffcd2957;
-				border-radius: 5px;
-				:global(.mdc-select__anchor) {
-					height: 2.5rem;
-				}
-				:global(label) {
-					color: #000 !important;
-					letter-spacing: 1px;
-				}
-			}
-		}
-
-		.conference-details-content-description {
-			display: flex;
-			flex-direction: column;
-			gap: 1rem;
-			.conference-details-content-description-content-comment {
-				width: 16rem;
-				height: 11rem;
-				background: rgba(217, 217, 217, 0.32);
-				border-radius: 10px;
-			}
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		.conference-details-content-description-content-comment {
+			width: 16rem;
+			height: 11rem;
+			background: rgba(217, 217, 217, 0.32);
+			border-radius: 10px;
 		}
 	}
 
-	@media (min-width: 1200px) {
+	// Desktop
+	@media (min-width: 1000px) {
 		hr {
 			border: 1px solid;
 		}
+
+		.conference-details-content-info-content {
+			grid-area: information;
+		}
+
+		.conference-details-content-info-status {
+			grid-area: status;
+		}
+
+		.conference-details-content-description {
+			grid-area: description;
+		}
+
 		.conference-details-content {
 			padding: 2.6rem 4rem !important;
+		}
 
-			.conference-details-content-info {
-				display: grid;
-				grid-template-columns: 1fr 1fr 0.5fr 1fr;
-				grid-template-areas: 'information information information status';
-				overflow: visible;
-				height: 14rem;
-				gap: 3rem;
+		.conference-details-content-info {
+			display: grid;
+			grid-template-columns: repeat(3, 1fr);
+			grid-template-areas: 'information information status';
+			height: 14rem;
+			gap: 3rem;
 
-				.conference-details-content-info-status {
-					.conference-details-content-info-status-my-status {
-						display: flex;
-					}
+			.conference-details-content-info-status {
+				.conference-details-content-info-status-registered {
+					display: grid;
+					grid-template-columns: repeat(2, 1fr);
+					border: 1px solid #000000;
+					border-radius: 10px;
 
-					.conference-details-content-info-status-registered {
-						display: grid;
-						grid-template-columns: 1fr 1fr;
-						border: 1px solid #000000;
-						border-radius: 10px;
-
-						div {
-							padding: 0.5rem 1rem;
-							&:first-of-type {
-								border-right: 1px solid #000000;
-							}
+					div {
+						padding: 0.5rem 1rem;
+						&:first-of-type {
+							border-right: 1px solid #000000;
 						}
 					}
 				}
 			}
+		}
 
-			.conference-details-content-description-container {
+		.conference-details-content-description-container {
+			display: grid;
+			grid-template-columns: repeat(3, 1fr);
+			grid-template-areas: 'description description .';
+
+			.conference-details-content-description {
 				display: grid;
-				grid-template-columns: 1fr 1fr 0.5fr 1fr;
-				grid-template-areas: 'description description description .';
-
-				.conference-details-content-description {
-					display: flex;
-					flex-direction: row !important;
-					justify-content: space-between;
-				}
+				grid-template-columns: 1fr 0.5fr;
+				justify-items: end;
 			}
 		}
 	}
