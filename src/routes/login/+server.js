7991edf4da6b8e2c5ffd2f327accf07d22d98ed1
Nacha Nanthawisit <nanthawisit.natacha@gmail.com>
2023-05-27 09:42:51 +0200

    adding exception handling when fetching token, user profile from google and cvpartner. Set userid and office id to undefined when user does not exists in cvpartner

--- src/routes/login/+server.js
+++ src/routes/login/+server.js
@@ -4,17 +4,17 @@ import { env } from '$env/dynamic/private';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET(event) {
-    const stringifiedParams = queryString.stringify({
-        client_id: env.GOOGLE_CLIENT_ID,
-        redirect_uri: `${env.ORIGIN}/oauth2/callback/google`,
-        scope: [
-            'https://www.googleapis.com/auth/userinfo.email',
-            'https://www.googleapis.com/auth/userinfo.profile',
-        ].join(' '),
-        response_type: 'code',
-        access_type: 'offline',
-        prompt: 'consent'
-    });
-    const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
-    throw redirect(307, googleLoginUrl);
+	const stringifiedParams = queryString.stringify({
+		client_id: env.GOOGLE_CLIENT_ID,
+		redirect_uri: `${env.ORIGIN}/oauth2/callback/google`,
+		scope: [
+			'https://www.googleapis.com/auth/userinfo.email',
+			'https://www.googleapis.com/auth/userinfo.profile'
+		].join(' '),
+		response_type: 'code',
+		access_type: 'offline',
+		prompt: 'consent'
+	});
+	const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
+	throw redirect(307, googleLoginUrl);
 }
--- src/routes/oauth2/callback/google/+server.js
+++ src/routes/oauth2/callback/google/+server.js
@@ -1,63 +1,82 @@
 import { env } from '$env/dynamic/private';
 import { dev } from '$app/environment';
-import { redirect } from '@sveltejs/kit';
+import { redirect, error } from '@sveltejs/kit';
 import { fetchUser } from '$lib/server/cvpartnerClient';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url, cookies }) {
-  const authCode = url.searchParams.get('code');
-  const tokenResponse = await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST',
-      body: JSON.stringify({
-          client_id: env.GOOGLE_CLIENT_ID,
-          client_secret: env.GOOGLE_CLIENT_SECRET,
-          redirect_uri: `${env.ORIGIN}/oauth2/callback/google`,
-          grant_type: 'authorization_code',
-          code: authCode,
-      })
-  });
-  const test = JSON.stringify({
-    client_id: env.GOOGLE_CLIENT_ID,
-    client_secret: env.GOOGLE_CLIENT_SECRET,
-    redirect_uri: `${env.ORIGIN}/oauth2/callback/google`,
-    grant_type: 'authorization_code',
-    code: authCode,
-});
-  const data = await tokenResponse.json();
-  if(data.error) {
-    console.error(data.error);
-    throw redirect(307, '/');
-  } else {
-    const profileResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${data.access_token}`,
-        },
-    });
-    const profileData = await profileResponse.json();
-    const cvpartner = await fetchUser(profileData.email);
-    const authInfo = {
-      isAuthenticated: true,
-      id: profileData.id,
-      name: `${profileData.given_name} ${profileData.family_name}`,
-      email: profileData.email,
-      profileImage: profileData.picture,
-      cvpartnerUserId: cvpartner.id,
-      cvpartnerOfficeId: cvpartner.office,
-      access_token: data.access_token,
-      expires_in: data.expires_in,
-      refresh_token: data.refresh_token,
-      scope: data.scope,
-      token_type: data.token_type,
-      id_token: data.id_token
-    };
-    cookies.set('session', JSON.stringify(authInfo), {
-        path: '/',
-        httpOnly: true,
-        sameSite: 'strict',
-        secure: !dev,
-        maxAge: authInfo.expires_in
-    });
-    throw redirect(307, '/');
-  }
+	const token = await getToken(url);
+	const profileData = await getUserProfile(token.access_token);
+	const cvpartnerData = await fetchUser(profileData.email);
+	const authInfo = createAuthInfo(profileData, cvpartnerData, token);
+	cookies.set('session', JSON.stringify(authInfo), {
+		path: '/',
+		httpOnly: true,
+		sameSite: 'strict',
+		secure: !dev,
+		maxAge: authInfo.expires_in
+	});
+
+	throw redirect(307, '/');
 }
+
+const getToken = async (url) => {
+	try {
+		const authCode = url.searchParams.get('code');
+		const tokenResponse = await fetch('https://oauth2.googleapis.com/token', {
+			method: 'POST',
+			body: JSON.stringify({
+				client_id: env.GOOGLE_CLIENT_ID,
+				client_secret: env.GOOGLE_CLIENT_SECRET,
+				redirect_uri: `${env.ORIGIN}/oauth2/callback/google`,
+				grant_type: 'authorization_code',
+				code: authCode
+			})
+		});
+
+		const data = await tokenResponse.json();
+
+		if (data.error) {
+			console.error(`GET Google token failed: ${data.error}`);
+			throw error(401, 'Failed to login');
+		}
+
+		return data;
+	} catch (err) {
+		console.error(`GET Google token failed: ${err}`);
+		throw error(401, 'Failed to login');
+	}
+};
+
+const getUserProfile = async (accessToken) => {
+	try {
+		const profileResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${accessToken}`
+			}
+		});
+		return await profileResponse.json();
+	} catch (err) {
+		console.error(`GET User profile failed: ${err}`);
+		throw error(500, 'Failed to get user profile');
+	}
+};
+
+const createAuthInfo = (profileData, cvpartnerData, token) => {
+	return {
+		isAuthenticated: true,
+		id: profileData.id,
+		name: `${profileData.given_name} ${profileData.family_name}`,
+		email: profileData.email,
+		profileImage: profileData.picture,
+		cvpartnerUserId: cvpartnerData ? cvpartnerData.id : undefined,
+		cvpartnerOfficeId: cvpartnerData ? cvpartnerData.office : undefined,
+		access_token: token.access_token,
+		expires_in: token.expires_in,
+		refresh_token: token.refresh_token,
+		scope: token.scope,
+		token_type: token.token_type,
+		id_token: token.id_token
+	};
+};
