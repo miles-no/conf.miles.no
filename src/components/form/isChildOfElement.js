d7459550a9d5298ba4af150f8e7855729c6c2859
Espen Norderud <espen.norderud@miles.no>
2023-05-31 12:11:11 +0200

    Remove unused functions

--- src/components/form/isChildOfElement.js
+++
@@ -1,8 +0,0 @@
-export function isChildOfElement(targetParentClass, element) {
-    if (element.classList.contains(targetParentClass)) {
-        return true;
-    }
-    return (element.parentElement)
-        ? isChildOfElement(targetParentClass, element.parentElement)
-        : false;
-}
