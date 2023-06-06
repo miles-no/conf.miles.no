a6cba8ea98102f66aedb0542171bf2d5334c8805
“natcha94” <nanthawisit.natacha@gmail.com>
2023-06-06 12:58:34 +0200

    Avatar component

---
+++ src/components/avatar/Avatar.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let imageUrl: string;
+	export let alt: string;
+	export let name: string | undefined = undefined;
+</script>
+
+<div class="avatar-container">
+	<img src={imageUrl} {alt} />
+	{#if name}
+		<p>{name}</p>
+	{/if}
+</div>
+
+<style lang="scss">
+	.avatar-container {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+
+		p {
+			margin: 0;
+		}
+
+		img {
+			border: 1px solid black;
+			border-radius: 50%;
+			height: 4rem;
+			width: 4rem;
+		}
+	}
+</style>
