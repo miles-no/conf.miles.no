a0afa6f88fd0d50a7dd58f8212f46813c4f5cb7b
Magnus Furnes Vadøy <magnus.f.vadoy@gmail.com>
2023-06-07 09:39:51 +0200

    Added link to 'Konferanser' for logged in users

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -9,6 +9,8 @@
 	<span aria-hidden={true} class="splitter">|</span>
 	<!-- Kommenterer ut denne til blir mer ferdig med denne delen -->
 	<!-- <a href="/protected">Administrasjon</a> | -->
+	<a href="/konferanser">Konferanser</a>
+	<span aria-hidden={true} class="splitter">|</span>
 	<a href="/logout">Logg ut</a>
 {:else}
 	<a href="/login">Login</a>
