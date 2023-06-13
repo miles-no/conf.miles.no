97acbae34acbf3aebe651ff2b25eb2758b2137b4
“natcha94” <nanthawisit.natacha@gmail.com>
2023-06-13 17:05:58 +0200

    responsive header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Button, { Label } from '@smui/button';
-	import Login from './Login.svelte';
-
+	import IconButton from '@smui/icon-button';
 	export let settings = [];
 	export let authInfo = {};
 	export let isDarkTheme: boolean | undefined = undefined;
@@ -15,39 +14,123 @@
 	</div>
 	<div class="header-container-nav-theme">
 		<nav>
-			<div class="links">
-				<ul class="navbar-nav m-0">
-					<li class="nav-item">
-						<Login {authInfo} />
+			{#if authInfo.isAuthenticated}
+				<span class="name">Hei {authInfo.name}. </span>
+				<ul>
+					<li>
+						<a href="/konferanser">Konferanser</a>
+					</li>
+					<li>
+						<a href="/logout">Logg ut</a>
 					</li>
 				</ul>
-			</div>
+			{:else}
+				<a class="login__link" href="/login">Login</a>
+			{/if}
 		</nav>
 		<Button class="header-container-nav-theme-btn" on:click={() => (isDarkTheme = !isDarkTheme)}>
 			<Label>{isDarkTheme ? 'Light mode' : 'Dark mode'}</Label>
 		</Button>
+		<IconButton
+			class="material-icons theme-icon-button"
+			aria-label={isDarkTheme ? 'Lys tema' : 'Mørk tema'}
+			on:click={() => (isDarkTheme = !isDarkTheme)}
+			>{isDarkTheme ? 'light_mode' : 'nightlight'}</IconButton
+		>
 	</div>
 </header>
 
-<style>
+<style lang="scss">
 	.header-container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+
+		.header-container-logo {
+			img {
+				width: 5rem;
+			}
+		}
+
+		.header-container-nav-theme {
+			display: flex;
+			gap: 0.5rem;
+			// align-items: end;
+
+			.name {
+				display: none;
+			}
+
+			nav {
+				display: flex;
+				gap: 0.5rem;
+				align-items: center;
+			}
+
+			ul {
+				display: flex;
+				gap: 0.5rem;
+				list-style: none;
+				padding: 0;
+				margin: 0;
+			}
+
+			li,
+			.login__link {
+				padding-right: 0.5rem;
+				border-right: 1px solid lightgray;
+			}
+		}
+
+		:global(.header-container-nav-theme-btn) {
+			display: none;
+		}
+
+		:global(.theme-icon-button) {
+			display: flex;
+			width: 2rem;
+		}
+	}
+
+	@media (min-width: 900px) {
+		.header-container-logo img {
+			width: 8rem !important;
+		}
+
+		.name {
+			display: unset !important;
+			padding-right: 0.5rem;
+			border-right: 1px solid lightgray;
+		}
+
+		:global(.header-container-nav-theme-btn) {
+			display: unset !important;
+		}
+
+		:global(.theme-icon-button) {
+			display: none !important;
+		}
+	}
+
+	/* .header-container {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
 		padding: 1em;
 		height: 4em;
-	}
+	} */
 
-	.miles-brand {
+	/* .miles-brand {
 		max-height: 30px;
-	}
+	} */
 
-	.header-container-nav-theme {
+	/* .header-container-nav-theme {
 		display: flex;
 		align-items: center;
-	}
+	} */
 
-	* :global(.header-container-nav-theme-btn) {
+	/* * :global(.header-container-nav-theme-btn) {
 		text-transform: none;
-	}
+	} */
 </style>
