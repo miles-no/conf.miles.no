ec816e0ccb2ea55437404de6c70020d41a550647
natcha94 <nanthawisit.natacha@gmail.com>
2023-06-20 17:31:45 +0200

    show filter button and modal on smaller screen (#78)

---
+++ src/components/checkbox/conference-category-checkbox-group/ConferenceCategoryCheckboxGroup.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import classNames from 'classnames';
+	import {
+		ConferenceCategory,
+		type ConferenceCategoryType
+	} from '../../../enums/conference-category';
+
+	export let selectedCategoryType: ConferenceCategoryType[] = [];
+	export let direction: 'vertical' | 'horizontal' = 'horizontal';
+	const options = Object.values(ConferenceCategory).map((category) => ({
+		name: category,
+		disabled: false
+	}));
+</script>
+
+<fieldset class={classNames('category-checkbox-container', direction)}>
+	<legend class="visuallyhidden">Velg kategori</legend>
+	{#each options as option}
+		<div>
+			<input
+				type="checkbox"
+				name={option.name}
+				id={option.name}
+				value={option.name}
+				disabled={option.disabled}
+				bind:group={selectedCategoryType}
+			/>
+			<label for={option.name}>{option.name}</label>
+		</div>
+	{/each}
+</fieldset>
+
+<style lang="scss">
+	@use '../../../styles/mixin' as *;
+
+	.visuallyhidden {
+		@include visuallyhidden();
+	}
+
+	.category-checkbox-container {
+		gap: 1rem;
+		font-size: 1rem;
+
+		&.horizontal {
+			display: flex;
+			flex-direction: row;
+			flex-wrap: wrap;
+		}
+
+		&.vertical {
+			display: flex;
+			flex-direction: column;
+			gap: 0.5rem;
+		}
+
+		> div {
+			display: flex;
+			align-items: center;
+			gap: 0.3rem;
+		}
+	}
+</style>
---
+++ src/components/modal/filter-conference-category-modal/FilterConferenceCategoryModal.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import Dialog from '@smui/dialog';
+	import Button, { Label } from '@smui/button';
+	import IconButton from '@smui/icon-button';
+
+	import ConferenceCategoryCheckboxGroup from '../../checkbox/conference-category-checkbox-group/ConferenceCategoryCheckboxGroup.svelte';
+	import type { ConferenceCategoryType } from '../../../enums/conference-category';
+
+	export let open: boolean = false;
+	export let selectedCategoryType: ConferenceCategoryType[] = [];
+	export let onSelect: (selected: ConferenceCategoryType[]) => void;
+
+	let selected = selectedCategoryType;
+</script>
+
+<Dialog bind:open>
+	<div class="filter-conference-category-modal-content-container">
+		<IconButton class="material-icons close-button" on:click={() => (open = !open)}
+			>close</IconButton
+		>
+		<div class="checkbox-container">
+			<h1>Velg kategori</h1>
+			<ConferenceCategoryCheckboxGroup bind:selectedCategoryType={selected} direction="vertical" />
+		</div>
+		<Button class="select-btn" variant="raised" on:click={() => onSelect(selected)}>
+			<Label>Bruk</Label>
+		</Button>
+	</div>
+</Dialog>
+
+<style lang="scss">
+	.filter-conference-category-modal-content-container {
+		padding: 0 1rem 1rem 1rem;
+		:global(.close-button) {
+			float: right;
+		}
+	}
+	.checkbox-container {
+		padding: 2rem 0.7rem 0.7rem 0.7rem;
+	}
+
+	:global(.select-btn) {
+		width: 100%;
+	}
+</style>
