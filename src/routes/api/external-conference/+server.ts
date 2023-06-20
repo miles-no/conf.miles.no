6bcb9911a81dd8bd647eb97d61bfe28962fb1d0a
natcha94 <nanthawisit.natacha@gmail.com>
2023-06-20 15:10:26 +0200

    changed dataset from test to production (#77)
    * changed dataset from test to production
    
    * moved update external conference to sanityclient

--- src/routes/api/external-conference/+server.ts
+++ src/routes/api/external-conference/+server.ts
@@ -1,37 +1,16 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import type { IExternalConference } from '../../../model/external-conference.js';
-import sanityClient from "@sanity/client";
-import {env} from "$env/dynamic/private";
-
-const client = sanityClient({
-	projectId: env?.PUBLIC_SANITY_PROJECTID ?? 'mhv8s2ia',
-	dataset: env?.PUBLIC_SANITY_DATASET ?? 'test',
-	token: env?.SANITY_TOKEN,
-	apiVersion: '2022-03-24',
-	useCdn: false
-});
+import { updateExternalConference } from '$lib/server/sanityClient.ts';
 
 // /api/external-conference PUT
 export const PUT = (async ({ request }) => {
 	try {
 		const externalConference = (await request.json()) as IExternalConference;
-		// imageUrl is not a part of external conference schema and updating external conference should not update its references to other schemas.
-		const { imageUrl, performances, image, _createdAt, _updatedAt, _id, _rev, location, ...rest } =
-			externalConference;
-
-		await client
-			.patch(externalConference._id)
-			.set({
-				...rest,
-				slug: { _type: 'slug', current: externalConference.slug }
-			})
-			.commit();
+		await updateExternalConference(externalConference);
 
 		return json({ success: true });
 	} catch (error) {
-		const { token, ...rest } = client.config();
-
-		console.error(`PUT /api/external-conference: ${error} ${JSON.stringify(rest)}`);
+		console.error(`PUT /api/external-conference: ${error}`);
 		return json({ success: false });
 	}
 }) satisfies RequestHandler;
