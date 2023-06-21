95464dbd8247c7b8865ffff2f2daf4c323198da1
Magnus Furnes Vadøy <magnus.f.vadoy@gmail.com>
2023-06-21 12:08:53 +0200

    fixed env path in migration and bumped sanity studio version

--- studio/migrations/migrateDocumentType.ts
+++ studio/migrations/migrateDocumentType.ts
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') });
 
 type Doc = {
 	_id: string;
