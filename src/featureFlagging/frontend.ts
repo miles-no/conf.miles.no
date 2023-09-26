fff389b7735e66b7cd742c4a5d63eb2a01cd5ead
Espen Norderud <espen.norderud@miles.no>
2023-09-26 15:06:52 +0200

    #166: Working feature toggle prototype

--- src/featureFlagging/frontend.ts
+++ src/featureFlagging/frontend.ts
@@ -5,6 +5,7 @@
  */
 
 
+
 /**  IMPORTANT! Over time, different feature flags may be scattered around the code base. Typescript help will come
  *   really handy for maintaining this, especially when it's time for cleanup. So:
  *   When setting up a new feature toggle,
@@ -12,13 +13,16 @@
  *   - and with a false value by that key in the object 'flags'
  *   - then use 'toggleByURIParams' to implement whatever logic for setting it - specifically and explicitly for each flag.
  */
-interface FrontendFeatureToggles {
+export interface FrontendFeatureToggles {
 	testFeature: boolean
 };
+export type FrontendFeatureToggleName = keyof FrontendFeatureToggles;
+
 export const flags: FrontendFeatureToggles = {
 	testFeature: false
 }
 
 export const toggleByURIParams = (searchParams: URLSearchParams) => {
 	flags.testFeature = !!searchParams.get("testFeature") ?? false;
+	console.log("flags:", flags);
 }
--- src/featureFlagging/server.ts
+++ src/featureFlagging/server.ts
@@ -5,6 +5,7 @@
  */
 
 
+
 /**  IMPORTANT! Over time, different feature flags may be scattered around the code base. Typescript help will come
  *   really handy for maintaining this, especially when it's time for cleanup. So:
  *   When setting up a new feature toggle,
@@ -13,9 +14,10 @@
  *   - then use 'setServersideToggle' to implement whatever logic for setting it - specifically and explicitly for each
  *   flag, one at a time in order to not risk exposing anything by inadvertently sending it in through an object.
  */
-interface ServersideFeatureToggles {
+export interface ServersideFeatureToggles {
 	// exampleFeatureName: boolean
 };
+export type ServersideFeatureToggleName = keyof ServersideFeatureToggles;
 export const flags: ServersideFeatureToggles = {
 	// exampleFeatureName: false
 }
